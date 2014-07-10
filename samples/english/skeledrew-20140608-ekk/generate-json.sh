4bd57035a22b77d12a5d39c781443bdef9c9c13a
git-burnik.chromium.org <burnik@google.com>
2014-07-10 12:59:16 +0200

    include samples.json for list of samples available to the demo

--- samples/english/skeledrew-20140608-ekk/generate-json.sh
+++ samples/english/skeledrew-20140608-ekk/generate-json.sh
@@ -8,7 +8,7 @@ fi
 dir="samples/english/skeledrew-20140608-ekk"
 echo "["; 
 for x in $(seq $1 $2); do 
-  echo -e "{\n\tfilename:'$dir/samples/english/skeledrew-20140608-ekk/wav/rb-$x.wav',\n\ttitle:'Sample $x',\n\ttext:'$(cat $dir/txt/rb-$x.txt)'\n},"; 
+  echo -e "{\n\t\"filename\":\"$dir/wav/rb-$x.wav\",\n\t\"title\":\"Sample $x\",\n\t\"text\":\"$(cat $dir/txt/rb-$x.txt)\"\n},"; 
 done ;
 echo "]"
 exit 0
